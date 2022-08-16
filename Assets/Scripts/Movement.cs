cc83376839a1667993980ae02b6d041f8d025e4c
İrem <105501017+iraska@users.noreply.github.com>
2022-08-16 17:21:35 +0300

    Movement is fixed.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,45 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
 
-    //Vector3 horizontalMove;
-    Vector3 forwardMove;
-
     Touch touch;
-    Vector3 position;
+    Rigidbody rb;
+    NavMeshAgent navMeshAgent;
+
     float width;
-    float height;
-
-    //Vector2 forwardMove;
-
+    float xPos;
+    float yPos = 0f;
+    float zPos = 1f;
 
     void Awake()
     {
+        xPos = 0f;
         width = (float)Screen.width / 2f;
-        height = (float)Screen.height / 2f;
 
-        position = new Vector3(0f, 0f, 0f);
+        rb = GetComponent<Rigidbody>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     void Update()
     {
-        //float horizontalInput = Input.GetAxis("Horizontal");
-        //horizontalMove = Vector3.right * horizontalInput;
-        //forwardMove = Vector3.forward * moveSpeed;
-        //Vector3 playerMove = forwardMove + horizontalMove;
+        TouchInput();
+    }
 
-        //transform.Translate(playerMove * Time.deltaTime);
+    void FixedUpdate() 
+    {
+        PlayerMove();
+    }
 
-        //forwardMove = Vector2.up * moveSpeed * Time.deltaTime;
-        //forwardMove = Vector3.forward * moveSpeed * Time.deltaTime;
-        //forwardMove = Vector3.forward * moveSpeed * Time.deltaTime;
-
-        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-
+    private void TouchInput()
+    {
         if (Input.touchCount > 0)   //touch the screen with one finger at least
         {
             touch = Input.GetTouch(0);
@@ -47,21 +44,19 @@ public class Movement : MonoBehaviour
             if (touch.phase == TouchPhase.Moved)
             {
                 Vector2 pos = touch.position;
-                pos.x = (pos.x - width) / width;
-                pos.y = (pos.y - height) / height;
-                position = new Vector3(pos.x, 0f, 0f);
-
-                // Position the cube.
-                transform.position = position;
-
-                
-
-                //transform.position = new Vector3(transform.position.x + touch.deltaPosition.x * moveSpeed, transform.position.y, transform.position.z + touch.deltaPosition.y * moveSpeed);
-
-                
-                //transform.position = new Vector3(touch.position.x, transform.position.y, forwardMove.z);
+                xPos = (pos.x - width) / width;
             }
 
+            else
+            {
+                xPos = 0;   //otherwise it keeps moving even if you don't touch it
+            }
         }
+    }
+
+    void PlayerMove()
+    {
+        Vector3 playerMove = new Vector3(xPos, yPos, zPos);
+        rb.MovePosition(transform.position + (playerMove * moveSpeed * Time.fixedDeltaTime));
     }
 }
