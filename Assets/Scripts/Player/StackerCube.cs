ebc9307275f31d635f08ab64123c2fcaaa0eb75b
İrem <105501017+iraska@users.noreply.github.com>
2022-08-29 17:55:51 +0300

    Double stack meshanism added.

--- Assets/Scripts/Player/StackerCube.cs
+++ Assets/Scripts/Player/StackerCube.cs
@@ -13,9 +13,12 @@ public class StackerCube : MonoBehaviour
     [SerializeField] GameObject floatingTextPrefab;
     //[SerializeField] GameObject ground;
 
-    Stack<GameObject> stack; //LIFO
+    Stack<GameObject> stack1; //LIFO
+    Stack<GameObject> stack2;
 
-    NavMeshAgent navMeshAgent;
+    NavMeshAgent leftNavMesh;
+    NavMeshAgent rightNavMesh;
+    NavMeshAgent navMesh;
     GameObject popedCube;
     GameObject stackedCube;
     AudioSource audioSource;
@@ -23,6 +26,8 @@ public class StackerCube : MonoBehaviour
     Color cubeColor;
     Renderer rend;
     TrailRenderer trail;
+    Transform leftChild;
+    Transform rightChild;
 
     Vector3 obstacleSize;
     Vector3 punchScale = new Vector3(.3f, .3f, .3f);
@@ -46,36 +51,83 @@ public class StackerCube : MonoBehaviour
         gameOverCanvas.enabled = false;
         successCanvas.enabled = false;
 
-        stack = new Stack<GameObject>();
+        stack1 = new Stack<GameObject>(); //for left cube
+        stack2 = new Stack<GameObject>(); //for right cube
 
-        navMeshAgent = GetComponent<NavMeshAgent>();
+        leftChild = GameObject.FindWithTag("LeftChild").GetComponent<Transform>();
+        rightChild = GameObject.FindWithTag("RightChild").GetComponent<Transform>();
+
+        //navMeshAgent = GetComponent<NavMeshAgent>();
+        leftNavMesh = leftChild.GetComponent<NavMeshAgent>();
+        rightNavMesh = rightChild.GetComponent<NavMeshAgent>();
+        navMesh = GetComponent<NavMeshAgent>();
+
         audioSource = GetComponent<AudioSource>();
         audioSource.Stop();
 
         trail = FindObjectOfType<TrailRenderer>();
         trail.enabled = false;
+
+        
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "StackableCube")
+        if (other.gameObject.tag == "LeftStackableCube" || other.gameObject.tag == "RightStackableCube")
         {
             StackedCube(other);
-            navMeshAgent.baseOffset++;
+            // if (leftNavMesh.baseOffset > rightNavMesh.baseOffset)
+            // {
+            //     navMesh.baseOffset = leftNavMesh.baseOffset;
+            //     rightNavMesh.baseOffset = navMesh.baseOffset; //otherwise the right cubes stay on the ground
+            // }
+            // else if (rightNavMesh.baseOffset > leftNavMesh.baseOffset)
+            // {
+            //     navMesh.baseOffset = rightNavMesh.baseOffset;
+            //     leftNavMesh.baseOffset = navMesh.baseOffset;
+            // }
+            // else if (leftNavMesh.baseOffset == rightNavMesh.baseOffset)
+            // {
+            //     navMesh.baseOffset = leftNavMesh.baseOffset;
+            // }
+            //navMeshAgent.baseOffset++;
+            // if (navMeshAgent.baseOffset == ((leftChild.position.y * 2) + .5f))
+            // {
+            //     navMeshAgent.baseOffset++;
+            // }
+            //if ()
+            //navMeshAgent.baseOffset++;
+            //if (leftChild.position.y )
+
+            // if (other.gameObject.tag == "LeftStackableCube")
+            // {
+            //     if (stack1.Count >= stack2.Count)
+            //     {
+            //         navMeshAgent.baseOffset++;
+            //     }
+            // }
+            // else if (other.gameObject.tag == "RightStackableCube")
+            // {
+            //     if (stack2.Count >= stack1.Count)
+            //     {
+            //         navMeshAgent.baseOffset++;
+            //     }
+            // }
+            // //else { return; }
         }
 
         else if (other.gameObject.tag == "ObstacleCube" || other.gameObject.tag == "Stair")
         {
             obstacleSize.y = other.gameObject.GetComponent<BoxCollider>().size.y;
 
-            if (obstacleSize.y <= stack.Count)
+            if (obstacleSize.y <= stack1.Count || obstacleSize.y <= stack2.Count)
             {
                 for (var i = 0; i < obstacleSize.y; i++)
                 {
                     PopedCube(other);
                 }
             }
-            else if (other.gameObject.tag == "ObstacleCube" && obstacleSize.y > stack.Count)
+            else if (other.gameObject.tag == "ObstacleCube" && (obstacleSize.y > stack1.Count || obstacleSize.y > stack2.Count))
             {
                 GetComponent<Movement>().enabled = false;
                 gameOverCanvas.enabled = true;
@@ -83,13 +135,20 @@ public class StackerCube : MonoBehaviour
             }
             else if (other.gameObject.tag == "Stair")
             {
-                if (stack.Count == 0)
+                if (stack1.Count == 0 || stack2.Count == 0)
                 {
                     GetComponent<Movement>().enabled = false;
                     successCanvas.enabled = true;
                     //FindObjectOfType<SceneLoader>().LoadNextLevel();
                 }
             }
+        }
+
+        else if (stack1.Count - stack2.Count >= 3 || stack2.Count - stack1.Count >= 3)
+        {
+            GetComponent<Movement>().enabled = false;
+            gameOverCanvas.enabled = true;
+            //TODO: ADD DEAD OR FALL ANIMATION
         }
     }
 
@@ -98,18 +157,123 @@ public class StackerCube : MonoBehaviour
         audioSource.Play();
 
         stackedCube = other.gameObject;
-        stack.Push(stackedCube);
 
-        SetPosition();
-        stackedCube.transform.position = new Vector3(xPos, yPos, zPos);
-        stackedCube.transform.parent = gameObject.transform; //SetParent is slightly slower
+        
 
-        stackedCube.tag = "Untagged"; //otherwise the stuck mechanic (while stuck.Count > 3) breaks cause triggers interact eachother
+        if (other.gameObject.tag == "LeftStackableCube")
+        {
+            stack1.Push(stackedCube);
+            SetPositionLeft();
+            //if (stack1.Count >= stack2.Count) { navMeshAgent.baseOffset++; }
+            stackedCube.transform.position = new Vector3(xPos, yPos, zPos);
+            stackedCube.transform.parent = leftChild;
+            stackedCube.tag = "Untagged"; //otherwise the stuck mechanic (while stuck.Count > 3) breaks cause triggers interact eachother
+            GetRandomEmoji();
+            PunchScaleCube(other);
+            SetTrailColor();
+            if (leftNavMesh.baseOffset <= stack1.Count + .5f)
+            {
+                leftNavMesh.baseOffset++;
+            }
+            else if (leftNavMesh.baseOffset > stack1.Count + .5f)
+            {
+                return;
+            }
+            //leftNavMesh.baseOffset++;
+            // if (leftNavMesh.baseOffset == stack1.Count + .5f)
+            // {
+            //     leftNavMesh.baseOffset++;
+            // }
+            // else if (leftNavMesh.baseOffset > stack1.Count + .5f)
+            // {
+            //     return;
+            // }
+        }
+        else if (other.gameObject.tag == "RightStackableCube")
+        {
+            stack2.Push(stackedCube);
+            SetPositionRight();
+            //if (stack2.Count >= stack1.Count) { navMeshAgent.baseOffset++; }
+            stackedCube.transform.position = new Vector3(xPos, yPos, zPos);
+            stackedCube.transform.parent = rightChild;
+            stackedCube.tag = "Untagged"; //otherwise the stuck mechanic (while stuck.Count > 3) breaks cause triggers interact eachother
+            GetRandomEmoji();
+            PunchScaleCube(other);
+            SetTrailColor();
+            
+            if (rightNavMesh.baseOffset <= (float)(stack2.Count + .5f))
+            {
+                rightNavMesh.baseOffset++;
+            }
+            else if (rightNavMesh.baseOffset > (float)(stack2.Count + .5f))
+            {
+                return;
+            }
+            // rightNavMesh.baseOffset++;
+            // if (rightNavMesh.baseOffset == stack2.Count + .5f)
+            // {
+            //     rightNavMesh.baseOffset++;
+            // }
+            // else if (rightNavMesh.baseOffset > stack2.Count + .5f)
+            // {
+            //     return;
+            // }
+        }
 
-        GetRandomEmoji();
-        PunchScaleCube(other);
-        SetTrailColor();
+        SetNavMeshBaseOffset();
+
+        //SetPosition();
+        //stackedCube.transform.position = new Vector3(xPos, yPos, zPos);
+        //stackedCube.transform.parent = gameObject.transform; //SetParent is slightly slower
+
+        //stackedCube.tag = "Untagged"; //otherwise the stuck mechanic (while stuck.Count > 3) breaks cause triggers interact eachother
+
+        // GetRandomEmoji();
+        // PunchScaleCube(other);
+        // SetTrailColor();
         //SetGroundColor();
+    }
+
+    void SetNavMeshBaseOffset()
+    {
+        if (leftNavMesh.baseOffset > rightNavMesh.baseOffset)
+        {
+            navMesh.baseOffset = leftNavMesh.baseOffset;
+            rightNavMesh.baseOffset = navMesh.baseOffset; //otherwise the right cubes stay on the ground
+            // if (leftNavMesh.baseOffset > stack1.Count + .5f)
+            // {
+            //     return;
+            // }
+        }
+        else if (rightNavMesh.baseOffset > leftNavMesh.baseOffset)
+        {
+            navMesh.baseOffset = rightNavMesh.baseOffset;
+            leftNavMesh.baseOffset = navMesh.baseOffset; //cs:242 and cs:251 are ridiculous when they work together !!!
+            // if (rightNavMesh.baseOffset > stack2.Count + .5f)
+            // {
+            //     return;
+            // }
+        }
+        else if (leftNavMesh.baseOffset == rightNavMesh.baseOffset)
+        {
+            navMesh.baseOffset = rightNavMesh.baseOffset;
+        }
+    }
+
+    void SetPositionRight()
+    {
+        xPos = rightChild.transform.position.x;
+        yPos = rightChild.transform.position.y;
+        zPos = rightChild.transform.position.z;
+        yPos -= stack2.Count;
+    }
+
+    void SetPositionLeft()
+    {
+        xPos = leftChild.transform.position.x;
+        yPos = leftChild.transform.position.y;
+        zPos = leftChild.transform.position.z;
+        yPos -= stack1.Count;
     }
 
     void SetTrailColor()
@@ -120,14 +284,14 @@ public class StackerCube : MonoBehaviour
         rend.material.color = cubeColor;
     }
 
-    void SetPosition()
-    {
-        xPos = transform.position.x;
-        yPos = transform.position.y;
-        zPos = transform.position.z;
-        yPos -= stack.Count;
+    // void SetPosition()
+    // {
+    //     xPos = transform.position.x;
+    //     yPos = transform.position.y;
+    //     zPos = transform.position.z;
+    //     yPos -= stack.Count;
 
-    }
+    // }
 
     void GetRandomEmoji()
     {
@@ -145,14 +309,25 @@ public class StackerCube : MonoBehaviour
 
     void PopedCube(Collider other)
     {
-        popedCube = stack.Pop();
+        if (other.gameObject.tag == "LeftStackableCube")
+        {
+            popedCube = stack1.Pop();
+            popedCube.transform.SetParent(null, true);
+        }
+
+        else if (other.gameObject.tag == "RightStackableCube")
+        {
+            popedCube = stack2.Pop();
+            popedCube.transform.SetParent(null, true);
+        }
+
         popedCube.transform.SetParent(null, true);
 
         if (other.gameObject.tag == "ObstacleCube")
         {
             Invoke("DelayPopedCube", delayInSeconds);    //the movement got worse with IEnumerator
 
-            if (stack.Count == 0)
+            if (stack1.Count == 0 || stack2.Count == 0)
             {
             trail.enabled = false;
             }
@@ -169,7 +344,7 @@ public class StackerCube : MonoBehaviour
 
     void DelayPopedCube()
     {
-        navMeshAgent.baseOffset--;
+        //navMeshAgent.baseOffset--;
         popedCube.GetComponent<BoxCollider>().enabled = false;
     }
 
