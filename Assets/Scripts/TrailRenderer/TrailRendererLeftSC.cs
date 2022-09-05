43dd3d439d08791124e415155a1a23a9e004285e
İrem <105501017+iraska@users.noreply.github.com>
2022-09-05 17:10:58 +0300

    Stacked cube bug is solved.

--- Assets/Scripts/TrailRenderer/TrailRendererLeftSC.cs
+++ Assets/Scripts/TrailRenderer/TrailRendererLeftSC.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class TrailRendererLeftSC : MonoBehaviour
 {
-    [SerializeField] GameObject player; //leftCube
-    StackerCube stackerCube;
+    [SerializeField] GameObject player; //Left cube
+    // StackerCube stackerCube;
+    LeftStacker stackerCube;
 
     void Start() 
     {
-        stackerCube = player.GetComponentInParent<StackerCube>();
+        // stackerCube = player.GetComponentInParent<StackerCube>();
+        stackerCube = player.GetComponent<LeftStacker>();
     }
 
     void Update() 
     {
-        stackerCube.SetPositionLeft();
+        // stackerCube.SetPositionLeft();
+        stackerCube.SetPosition();
     }
 }
--- Assets/Scripts/TrailRenderer/TrailRendererRightSC.cs
+++ Assets/Scripts/TrailRenderer/TrailRendererRightSC.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class TrailRendererRightSC : MonoBehaviour
 {
-    [SerializeField] GameObject player; //rightCube
-    StackerCube stackerCube;
+    [SerializeField] GameObject player; //Right cube
+    // StackerCube stackerCube;
+    RightStacker stackerCube;
 
     void Start() 
     {
-        stackerCube = player.GetComponentInParent<StackerCube>();
+        // stackerCube = player.GetComponentInParent<StackerCube>();
+        stackerCube = player.GetComponent<RightStacker>();
     }
 
     void Update() 
     {
-        stackerCube.SetPositionRight();
+        // stackerCube.SetPositionRight();
+        stackerCube.SetPosition();
     }
 }
